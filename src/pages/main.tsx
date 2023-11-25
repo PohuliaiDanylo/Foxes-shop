c3c3423ddce53fef4b150750f325f270468085cd
geelona <mr.geelona@gmail.com>
2023-11-25 20:01:04 +0200

    done with a main page

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,7 +1,76 @@
+import { CustomLink } from "../components/customLink";
+
 function Main() {
   return (
-    <div>
-      <h1>Main</h1>
+    <div className="main__container">
+      <div className="main__container__discover">
+        <img
+          className="main__container__discover__img"
+          src="/assets/mainPage/background.png"
+          alt=""
+        />
+        <div className="main__container__discover__text">
+          <h1 className="font-serif text-4xl font-bold">Discover foxlife</h1>
+          <p className="text-sm">
+            Lorem ipsum dolor sit amet consectetur. Phar eget turpis sem
+            ultricies dolor sit amet consectetur.
+          </p>
+        </div>
+      </div>
+
+      <div className="main__container__foods">
+        <div className="main__container__foods__food">
+          <div className="main__container__foods__food__text">
+            <h1>#Food</h1>
+            <p>Lorem ipsum dolor sit amet ipsum</p>
+          </div>
+          <img
+            className="main__container__foods__food__img"
+            src="/assets/mainPage/img1.png"
+            alt=""
+          />
+        </div>
+
+        <div className="main__container__foods__food">
+          <div className="main__container__foods__food__text">
+            <h1>#Food</h1>
+            <p>Lorem ipsum dolor sit amet ipsum</p>
+          </div>
+          <img
+            className="main__container__foods__food__img"
+            src="/assets/mainPage/img3.png"
+            alt=""
+          />
+        </div>
+
+        <div className="main__container__foods__food">
+          <div className="main__container__foods__food__text">
+            <h1>#Food</h1>
+            <p>Lorem ipsum dolor sit amet ipsum</p>
+          </div>
+          <img
+            className="main__container__foods__food__img"
+            src="/assets/mainPage/img2.png"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <CustomLink to="/items">
+        <div className="main__container__all-foxes-button">All foxes</div>
+      </CustomLink>
+
+      <div className="main__container__subscribe flex flex-col items-center p-5 gap-3 pb-3 mb-8">
+        <h1 className="text-xl font-bold font-mono">Join our newsletter</h1>
+        <p className="text-center">
+          Lorem ipsum dolor sit amet consectetur. Molestie turpis quis turpis
+          fermentum egestas bibendum.Lorem ipsum dolor sit amet consectetur.{" "}
+        </p>
+        <div className="main__container__subscribe__input flex flex-col gap-3">
+          <input className="text-xl" type="text" placeholder="Enter email" />
+          <button className="text-xl">Subscribe</button>
+        </div>
+      </div>
     </div>
   );
 }
